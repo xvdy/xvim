ae5d15a5da0ddd4da20880c8a748a3d5023126eb
xudongyang <xudongyang@xiaomi.com>
2020-08-15 16:33:36 +0800

    rm init.sh

--- init.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -d "~/.vim" ]; then
-  mv ~/.vim ~/.vim.bk.`date +%F`
-fi
-
-git clone git@github.com:xvdy/xvim.git ~/.vim/
-echo 'source ~/.vim/xvim/vimrc' > ~/.vimrc
-mv ~/.vim/xvim/bundles ~/.vim/
