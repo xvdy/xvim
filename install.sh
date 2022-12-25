930ad017f90eaece3c8c4aa6e263fde317f57eda
xudongyang <xudongyang@xiaomi.com>
2022-12-25 14:54:32 +0800

    update install

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
+set -e
+
 gitRepo="https://gitee.com/xdy/xvim.git"
 pluginType="default"
 
-if test $1 = "github" -o $2 = "github" ; then
+if [ "$1" = "github" ] || [ "$2" = "github" ] ; then
     gitRepo="https://github.com/xvdy/xvim.git"
 fi
-if test $1 = "full" -o $2 = "full" ; then
+
+if [ "$1" = "full" ] || [ "$2" = "full" ] ; then
     pluginType="full"
 fi
 
 echo $gitRepo $pluginType
 
-if [ ! -d "~/.vim" ]; then
+if [ -d ~/.vim ]; then
+  echo "backup vim"
+  rm -rf ~/.vim.bk.`date +%F`
   mv ~/.vim ~/.vim.bk.`date +%F`
 fi
 
@@ -18,8 +23,9 @@ mkdir -p ~/.vim/xvim
 git clone $gitRepo  ~/.vim/xvim
 echo 'source ~/.vim/xvim/vimrc' > ~/.vimrc
 
-if [ $pluginType == "full" ]
+if [ "$pluginType" = "full" ] ; then
     vim -E -c PlugInstall -c q
 else
     cp -R ~/.vim/xvim/bundles ~/.vim/
 fi
+
